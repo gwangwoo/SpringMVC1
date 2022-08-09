471ff8020c94d4654769722978c844838c53838a
gwangwoo <rhkddn5161@naver.com>
2022-08-09 21:37:02 +0900

    first-commit

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+/**
+  http://localhost:8080/request-param?username=kim&age=20
+ */
+@WebServlet(name = "reqeustParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String[] usernames = req.getParameterValues("username");
+    String age = req.getParameter("age");
+    Arrays.stream(usernames).forEach(System.out::println);
+    System.out.println("age = " + age);
+
+    resp.getWriter().write("ok");
+  }
+}
